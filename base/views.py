47b632edfba4a6d8a1d883237cadf0465bed843f
Albinis <ruddaku@gmail.com>
2021-10-11 20:32:36 +0200

    Added dynamic urls

--- base/views.py
+++ base/views.py
@@ -3,8 +3,21 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
+rooms = [
+    {'id': 1, 'name': 'Python'},
+    {'id': 2, 'name': 'Javascript'},
+    {'id': 3, 'name': 'MySQL'},
+    {'id': 4, 'name': 'Java'},
+]
 
-def room(request):
-    return render(request, 'room.html')
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
